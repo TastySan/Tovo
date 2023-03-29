744eb2a53f8e6f01f9a7191ce3fb9a5558058509
Paulus,S.C. (Sander) <s.c.paulus@students.uu.nl>
2023-03-29 21:00:20 +0200

    Add project files.

---
+++ Tovo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tovo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
